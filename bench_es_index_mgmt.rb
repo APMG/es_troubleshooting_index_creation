9cf4155aac5d3ec6764af09ffbbf3754fed87238
Stevenson <astevenson@2bastevensonosx.mpr.org>
2020-05-01 16:36:59 -0500

    initial commit

---
+++ bench_es_index_mgmt.rb
@@ -0,0 +1,43 @@
+require 'benchmark/ips'
+require 'elasticsearch'
+
+client = Elasticsearch::Client.new(url: 'http://localhost:9200')
+
+index_configs = (1..20).map do |i|
+  alias_name = "test_index_#{i}"
+  name = "test_#{i}_index_#{i}"
+  config = {
+    aliases: { alias_name => {} },
+    settings: {
+      number_of_shards: 1,
+      number_of_replicas: 0
+    }
+  }
+
+  {name: name, body: config}
+end
+
+def delete_my_indices!
+  client = Elasticsearch::Client.new(url: 'http://localhost:9200')
+  all_indices = client.indices.get_alias().keys
+  my_indices = all_indices.grep(/test_/)
+  client.indices.delete(index: my_indices) if my_indices.any?
+end
+
+
+Benchmark.ips do |x|
+  # Configure the number of seconds used during
+  # the warmup phase (default 2) and calculation phase (default 5)
+  x.config(:time => 15, :warmup => 5)
+
+  x.report("create 20 indices") do
+    delete_my_indices!
+    index_configs.each do |conf|
+      client.indices.create(index: conf[:name], body: conf[:body])
+    end
+  end
+
+  # Compare the iterations per second of the various reports!
+  x.compare!
+end
+delete_my_indices!
